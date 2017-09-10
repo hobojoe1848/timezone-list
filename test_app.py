5d874da1ab1a7e55dba3e740be5d521f3bafc4ea
bbelderbos <bobbelderbos@gmail.com>
2017-09-10 23:34:36 +0200

    add tests (issue 1)

---
+++ test_app.py
@@ -0,0 +1,31 @@
+import unittest
+
+import pendulum
+
+from timezones import create_list, get_tz_time
+
+
+class TestTzApp(unittest.TestCase):
+
+    def test_create_list(self):
+        tz_list = create_list()
+        self.assertEqual(len(tz_list), 593)
+        self.assertIn('Australia/Sydney', tz_list)
+        self.assertIn('Europe/Madrid', tz_list)
+        self.assertNotIn('Europe/Alicante', tz_list)
+
+    def test_get_tz(self):
+        utc = pendulum.now(tz='UTC')
+        cet_str = get_tz_time('Europe/Amsterdam')
+        # could get a one off second
+        cet_dt = pendulum.parse(cet_str).add(seconds=2)
+        diff = cet_dt.diff(utc).in_hours()
+        self.assertEqual(diff, 2)
+        aus_str = get_tz_time('Australia/Sydney')
+        aus_dt = pendulum.parse(aus_str).add(seconds=2)
+        diff = aus_dt.diff(utc).in_hours()
+        self.assertEqual(diff, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
